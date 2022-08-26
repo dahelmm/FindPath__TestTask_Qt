d48f79706c3f8ed63002014cd01d5643a1d0748d
dahelmm <dahelmm@mail.ru>
2022-08-26 23:52:10 +0300

    Сделал сетку и начальное GUI

--- findpathmain.cpp
+++ findpathmain.cpp
@@ -1,11 +1,18 @@
 #include "findpathmain.h"
 #include "ui_findpathmain.h"
 
+#include <QMessageBox>
+
 FindPathMain::FindPathMain(QWidget *parent)
   : QMainWindow(parent)
   , ui(new Ui::FindPathMain)
+  , m_width(0)
+  , m_height(0)
 {
   ui->setupUi(this);
+  scene = new QGraphicsScene(this);
+  ui->gV_field->setScene(scene);
+
 }
 
 FindPathMain::~FindPathMain()
@@ -13,3 +20,29 @@ FindPathMain::~FindPathMain()
   delete ui;
 }
 
+
+void FindPathMain::on_pB_generate_clicked()
+{
+    if(ui->lE_width->text().toUInt() && ui->lE_height->text().toUInt())
+    {
+      m_width = ui->lE_width->text().toUInt();
+      m_height = ui->lE_height->text().toUInt();
+    }
+    else
+    {
+      QMessageBox::critical(this, "Ошибка", "Введите значения ширины и высоты");
+      return;
+    }
+
+    int stepHeight = ui->gV_field->height() / m_height;
+    int stepWidth = ui->gV_field->width() / m_width;
+    QPen pen(Qt::SolidLine);
+    QBrush brush(QColor(255,255,255));
+    for(int i = 0; i < m_height; i++)
+    {
+      for(int j = 0; j < m_width; j++)
+      {
+        QGraphicsItem *item = scene->addRect(QRect(j*stepWidth, i*stepHeight, stepWidth, stepHeight),pen, brush);
+      }
+    }
+}
--- findpathmain.h
+++ findpathmain.h
@@ -2,6 +2,8 @@
 #define FINDPATHMAIN_H
 
 #include <QMainWindow>
+#include <QGraphicsItem>
+#include <QGraphicsScene>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class FindPathMain; }
@@ -15,7 +17,15 @@ public:
   FindPathMain(QWidget *parent = nullptr);
   ~FindPathMain();
 
+private slots:
+  void on_pB_generate_clicked();
+
 private:
   Ui::FindPathMain *ui;
+  QGraphicsScene *scene;
+
+  int m_width;
+  int m_height;
+
 };
 #endif // FINDPATHMAIN_H
